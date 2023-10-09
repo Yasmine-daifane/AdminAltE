ecd43da4c1874e414ed6393973af9e8dfa943d83
yasmine <110662044+Yasmine-daifane@users.noreply.github.com>
2023-10-09 08:55:22 -0700

    push

--- Prototype-team/Views/clients/Ajouter.php
+++ Prototype-team/Views/clients/Ajouter.php
@@ -1,30 +1,27 @@
+<?php
+define('__ROOT__', dirname(dirname(__FILE__)));
+?>
 <!DOCTYPE html>
 <html lang="fr">
 <?php
-include_once(__ROOT__ . "/Views/Layout/head.php");
+include_once(__ROOT__ . "/Layout/head.php");
 ?>
 
 <body class="hold-transition sidebar-mini layout-fixed">
     <div class="wrapper">
         <!-- Preloader -->
         <?php
-        // include "Views/Layout/preloader.php";
-        include_once(__ROOT__ . "/Views/Layout/preloader.php");
-
+        include_once(__ROOT__ . "/Layout/preloader.php");
         ?>
         <!-- Navbar -->
         <?php
-        // include "Views/Layout/navbare.php";
-        include_once(__ROOT__ . "/Views/Layout/navbare.php");
-
+        include_once(__ROOT__ . "/Layout/navbare.php");
         ?>
         <!-- /.navbar -->
 
         <!-- Main Sidebar Container -->
         <?php
-        // include "Views/Layout/sidebare.php";
-        include_once(__ROOT__ . "/Views/Layout/sidebare.php");
-
+        include_once(__ROOT__ . "/Layout/sidebare.php");
         ?>
         <!-- /.sidebar -->
         <div class="content-wrapper" style="min-height: 1604.61px;">
@@ -32,12 +29,12 @@ include_once(__ROOT__ . "/Views/Layout/head.php");
                 <div class="container-fluid">
                     <div class="row mb-2">
                         <div class="col-sm-6">
-                            <h1>Client Add</h1>
+                            <h1> Add Competences</h1>
                         </div>
                         <div class="col-sm-6">
                             <ol class="breadcrumb float-sm-right">
                                 <li class="breadcrumb-item"><a href="#">Home</a></li>
-                                <li class="breadcrumb-item active">Client Add</li>
+                                <li class="breadcrumb-item active"> Add Competences</li>
                             </ol>
                         </div>
                     </div>
@@ -50,7 +47,7 @@ include_once(__ROOT__ . "/Views/Layout/head.php");
                         <div class="col">
                             <div class="card card-primary">
                                 <div class="card-header">
-                                    <h3 class="card-title">Add Client</h3>
+                                    <h3 class="card-title">Add Competences</h3>
                                     <div class="card-tools">
                                         <button type="button" class="btn btn-tool" data-card-widget="collapse"
                                             title="Collapse">
@@ -60,24 +57,31 @@ include_once(__ROOT__ . "/Views/Layout/head.php");
                                 </div>
                                 <div class="card-body">
                                     <div class="form-group">
-                                        <label for="inputName">Client Name</label>
+                                        <label for="inputName"> Competences Nom</label>
                                         <input name="nom" type="text" id="inputName" class="form-control">
                                     </div>
                                     <div class="form-group">
-                                        <label for="inputEmail">Client Email</label>
+                                        <label for="inputEmail"> Competences Code  </label>
                                         <textarea name="email" id="inputEmail" class="form-control"
                                             rows="4"></textarea>
                                     </div>
+
+                                    <div class="form-group">
+                                        <label for="inputEmail"> Description   </label>
+                                        <textarea name="email" id="inputEmail" class="form-control"
+                                            rows="4"></textarea>
+                                    </div>
+
+
+                                   
                                 </div>
-
                             </div>
-
                         </div>
                     </div>
                     <div class="row">
                         <div class="col-12">
                             <a href="index.php" class="btn btn-secondary">Cancel</a>
-                            <input type="submit" value="Create new Client" class="btn btn-success float-right">
+                           
                         </div>
                     </div>
                 </form>
@@ -85,11 +89,11 @@ include_once(__ROOT__ . "/Views/Layout/head.php");
         </div>
     </div>
     <?php
-    include_once(__ROOT__ . "/Views/Layout/footer.php");
+    include_once(__ROOT__ . "/Layout/footer.php");
     ?>
 </body>
 <?php
-include_once(__ROOT__ . "/Views/Layout/links.php");
+include_once(__ROOT__ . "/Layout/links.php");
 ?>
 
 </html>
