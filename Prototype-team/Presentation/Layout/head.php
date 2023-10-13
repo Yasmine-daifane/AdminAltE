5d6ce4400224e1f76d0826b63a7559b64de2ecbe
yasmine <110662044+Yasmine-daifane@users.noreply.github.com>
2023-10-13 16:59:11 +0100

    push fin

--- Prototype-team/Presentation/Layout/head.php
+++ Prototype-team/Presentation/Layout/head.php
@@ -3,8 +3,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Gestion des Competences</title>
     <!-- Google Font: Source Sans Pro -->
-    <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
-
+    <script src="https://cdn.tiny.cloud/1/rstxdbg3rllarurra768zcwtwphhxqlxqvnhebaxc017ot19/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
 
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
